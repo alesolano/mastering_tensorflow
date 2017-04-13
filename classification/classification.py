7af70f61d37d27ef4bfde5ce282fde4df183c01c
Ale Solano <asolanopf@gmail.com>
2017-04-13 16:19:42 +0200

    update classification

--- classification/classification.py
+++ classification/classification.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import numpy as np
 
-from helper import split_data, get_batches
+from helper import get_data, split_data, get_batches
 
 
 # MISSION:
@@ -9,10 +9,10 @@ from helper import split_data, get_batches
 
 
 ############ BUILDING THE GRAPH ############
+
 # HYPERPARAMTERS
-#learning_rate = 0.5 # Terribly important hyperparameter. It can make your net go totally crazy.
-batch_size = 100
-epochs = 200
+batch_size = 50
+epochs = 10
 
 # MODEL
 x = tf.placeholder(tf.float32, [None, 2])
@@ -22,47 +22,49 @@ W_h = tf.Variable(tf.truncated_normal([2, 1], stddev=0.05))
 b_h = tf.Variable(tf.random_normal([1]))
 
 hidden = tf.add(tf.matmul(x, W_h), b_h)
-hidden = tf.sigmoid(hidden)
+#hidden = tf.sigmoid(hidden) # We don't need to add non-linear behaviors to our network.
 
-W_o = tf.Variable(tf.truncated_normal([1,2], stddev=0.05))
+W_o = tf.Variable(tf.truncated_normal([1, 2], stddev=0.05))
 b_o = tf.Variable(tf.random_normal([1]))
 
 logits = tf.add(tf.matmul(hidden, W_o), b_o)
 output = tf.nn.softmax(logits)
 
-y = tf.placeholder(tf.float32, [None, 2]) 
+y = tf.placeholder(tf.float32, [None, 2])
 
 # Training
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y))
 optimizer = tf.train.AdamOptimizer().minimize(cost)
 
 # Accuracy
-correct_pred = tf.equal(tf.argmax(logits, axis= 1), tf.argmax(y, axis=1)) # Not very sure about the axis
+correct_pred = tf.equal(tf.argmax(logits, axis=1), tf.argmax(y, axis=1))
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
 
 ############ DATA ############
-input1 = np.random.randint(10, size=10000) # shape := [1, 10000]
-input2 = np.random.randint(10, size=10000) # shape := [1, 10000]
-inputs = np.stack((input1, input2), axis=-1) # shape := [10000, 2] -> same as placeholder 'x'
+# inputs: two integers
+# targets: [0, 1] if the sum is higher than 10. [1, 0] if the sum is lower than 10.
 
-target1 = ((input1 + input2) > 10).astype(int) # shape := [10000,]
-target1 = np.reshape(target1, [-1, 1]) # shape := [10000, 1]
-target2 = 1 - target1 # shape := [10000, 1]
-targets = np.reshape(np.stack((target1, target2), axis=-1), [-1, 2]) # shape := [10000, 2] -> same as placeholder 'y'
+inputs, targets = get_data(max_int=10, size=10000)
 
+# preprocessing: normalize inputs to be between -1 and 1.
+inputs = (inputs-5)/5
+# TODO: make a preprocessing helper function, substracting the mean and dividing by the max value
+
+# split train and test data
+train_inputs, test_inputs, train_targets, test_targets = split_data(inputs, targets)
 
 
 ############ SESSION ############
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
-    train_inputs, test_inputs, train_targets, test_targets = split_data(inputs, targets)
-    batch_x, batch_y = get_batches(train_inputs, train_targets, batch_size)
-
     # TRAINING
     # I don't know very well what epochs are
     for epoch in range(epochs):
+
+        batch_x, batch_y = get_batches(train_inputs, train_targets, batch_size)
+
         for batch in range(train_inputs.shape[0]//batch_size):
 
             sess.run(optimizer, feed_dict={
@@ -89,9 +91,16 @@ with tf.Session() as sess:
         })
     print('Testing Accuracy: {}'.format(test_accuracy))
 
+
     final_test = sess.run(output, feed_dict={
-        x: np.array([[5, 3], [7, 6], [10, 10]])
+        x: (np.array([[5, 3], [7, 6], [10, 10]])-5)/5
         })
     print(final_test)
 
-
+    print("\nHidden layer weights and bias")
+    print(sess.run(W_h))
+    print(sess.run(b_h))
+    print("Output layer weights and bias")
+    print(sess.run(W_o))
+    print(sess.run(b_o))
+    
--- classification/helper.py
+++ classification/helper.py
@@ -1,9 +1,23 @@
 import numpy as np
 
 
-def split_data(inputs, targets):
-    size = inputs.shape[0]
-    train_size = int(np.ceil(size*.75))
+def get_data(max_int=10, size=10000):
+    input1 = np.random.randint(max_int+1, size=size) # shape := [1, 10000]
+    input2 = np.random.randint(max_int+1, size=size) # shape := [1, 10000]
+    inputs = np.stack((input1, input2), axis=-1) # shape := [10000, 2] -> same as placeholder 'x'
+    
+    targets = ((input1 + input2) > 10).astype(int) # shape := [10000,]
+    # one-hot encoding
+    targets = np.eye(2)[targets] # shape := [10000, 2] -> same as placeholder 'y'
+
+    return inputs, targets
+
+
+def split_data(inputs, targets, train_percentage=.75):
+    assert (0 <= train_percentage) and (train_percentage <= 1) # Train percentage must be within [0,1]
+
+    size = len(inputs) # length of the data
+    train_size = int(np.ceil(size*train_percentage))
     test_size = size - train_size
 
     train_inputs = inputs[:train_size, :]
@@ -17,7 +31,13 @@ def split_data(inputs, targets):
 def get_batches(inputs, targets, batch_size):
     # TOIMPROVE: Now, if I can't fill the last batch with enough data, I'm droping it.
     # I'm not using all the information that I have
-    num_batches = inputs.shape[0]//batch_size
+    
+    # batches have to be picked randomly, so we need to shuffle the data
+    p = np.random.permutation(len(inputs))
+    inputs = inputs[p]
+    targets = targets[p]
+
+    num_batches = len(inputs)//batch_size
 
     batch_x = np.zeros([num_batches, batch_size, 2]) # batch_x[0] has to be the same shape than placeholder x
     batch_y = np.zeros([num_batches, batch_size, 2]) # batch_y[0] has to be the same shape than placeholder y
